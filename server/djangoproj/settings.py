f41805e95b383538cf353d069aa0819ffe63697d
Ashley Christie <achristie0319@gmail.com>
2024-11-13 20:42:29 -0500

    lint changes 4

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,11 +29,11 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-    'https://achristie031-8000.' + 
-    'theiadockernext-0-labs-prod-theiak8s-4-tor01' + 
-    '.proxy.cognitiveclass.ai']
+                 'https://achristie031-8000.' +
+                 'theiadockernext-0-labs-prod-theiak8s-4-tor01' +
+                 '.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://achristie031-8000.' +
-                        'theiadockernext-0-labs-prod-theiak8s-4-tor01.' + 
+                        'theiadockernext-0-labs-prod-theiak8s-4-tor01.' +
                         'proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
@@ -99,7 +99,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
